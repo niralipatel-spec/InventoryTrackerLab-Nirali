6abbbe1159e40485c80e769da2463d8f816b7750
Nirali Patel <pate5855@sheridancollege.ca>
2026-02-19 16:57:31 -0500

    Project setup by Nirali Patel

---
+++ src/InventoryTracker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Nirali
+ */
+public class InventoryTracker {
+
+    public void displayInfo() {
+        System.out.println("InventoryTracker running...");
+    }
+}
+
